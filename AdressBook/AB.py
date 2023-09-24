432e9a9c0c983da9e9693345b88ffec3060660bd
DSofiya <134394822+DSofiya@users.noreply.github.com>
2023-09-24 12:32:24 +0300

    Update AB.py

--- AdressBook/AB.py
+++ AdressBook/AB.py
@@ -1,244 +1,252 @@
-from collections import UserDict
+from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 import os
-import dill as pickle
 import re
-from dateparser import parse
+import dill as pickle
+from dateparser import parse as dateparse
 from prompt_toolkit import prompt
+
+
 from .prompt_tool import Completer, RainbowLetter
 
 
-class Field:
+class Field(ABC):
     def __init__(self, some_value):
-        self._value = None
-        self.value = some_value
+        self._value = self.validate(some_value)
 
     @property
     def value(self):
         return self._value
 
-    @value.setter
-    def value(self, value):
-        self._value = value
+    @abstractmethod
+    def validate(self, value):
+        pass
 
     def __str__(self):
-        return f'{self.value}'
+        return str(self.value)
 
 
-class Name(Field):
-    pass
+class NameField(Field):
+    def validate(self, value):
+        if not value.isalpha():
+            raise ValueError("Name should consist of letters only")
+        return value
 
 
-class Phone(Field):
-    @Field.value.setter
-    def value(self, value):
+class PhoneField(Field):
+    def validate(self, value):
         for i in value:
-            if i.isdigit() or i in '+()':
-                continue
-            else:
-                raise TypeError
-        self._value = value
+            if i not in '0123456789+()':
+                raise ValueError("Invalid characters in phone number")
+        return value
 
 
-class Birthday(Field):
-    def valid_date(self, value: str):
-        try:
-            # obj_datetime = parse(value)
-            obj_datetime = datetime.strptime(value, '%Y-%m-%d')
+class BirthdayField(Field):
+    def validate(self, value):
+        obj_datetime = dateparse(value)
+        if obj_datetime:
             return obj_datetime.date()
-        except KeyError:
-            raise TypeError('Wrong data type. Try "yyyy-mm-dd"')
-
-    @Field.value.setter
-    def value(self, value):
-        self._value = self.valid_date(value)
-
-
-class Email(Field):
-    @Field.value.setter
-    def value(self, value: str):
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-        if re.fullmatch(regex, value):
-            self._value = value
         else:
-            raise TypeError(f'Wrong email')
+            raise ValueError('Invalid date format')
 
 
-class Adress(Field):
-    pass
+class EmailField(Field):
+    def validate(self, value):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.fullmatch(regex, value):
+            raise ValueError('Invalid email format')
+        return value
+
+
+class AddressField(Field):
+    def validate(self, value):
+        valid_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-,.")
+        if not all(char in valid_characters for char in value):
+            raise ValueError("Address should consist of letters, digits, hyphen, comma, and period")
+        return value
 
 
 class Record:
-    def __init__(self, name: Name, phone: Phone, birthday: Birthday, email: Email, adress=None):
+    def __init__(self, name: NameField, phone: PhoneField, birthday: BirthdayField, email: EmailField, address=None):
         self.name = name
-        self.phones = []
-        if phone:
-            self.phones.append(phone)
-            self.phone = phone
+        self.phones = [phone] if phone else []
         self.birthday = birthday
         self.email = email
-        if adress:
-            self.adress = adress
+        self.address = address
 
     def add_phone(self, phone):
-        phone_number = Phone(phone)
+        phone_number = PhoneField(phone)
         if phone_number not in self.phones:
             self.phones.append(phone_number)
 
     def remove_phone(self, phone):
-        phone_obj = Phone(phone)
+        phone_obj = PhoneField(phone)
         if phone_obj in self.phones:
             self.phones.remove(phone_obj)
-
-    def edit_phone(self, old_phone, new_phone):
-        self.remove_phone(old_phone)
-        self.add_phone(new_phone)
 
     def days_to_birthday(self):
         pass
 
 
-class AddressBook(UserDict):
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
     def add_record(self, record: Record):
         self.data[record.name.value] = record
 
-    def update_record(self, record: Record):
-        self.data[record.name.value] = record
-
     def delete_record(self, name):
-        self.data.pop(name)
+        self.data.pop(name, None)
 
-    def find_record(self, name):
-        return self.data.get(name)
-
-    def __init__(self):
-        super().__init__()
-
-    def dump(self):
-        with open('AdressBook.bin', 'wb') as file:
+    def dump(self, filename='AddressBook.bin'):
+        with open(filename, 'wb') as file:
             pickle.dump(self.data, file)
 
-    def load(self):
-        with open('AdressBook.bin', 'rb') as file:
-            self.data = pickle.load(file)
-        
+    def load(self, filename='AddressBook.bin'):
+        if os.path.exists(filename):
+            with open(filename, 'rb') as file:
+                self.data = pickle.load(file)
+
+
 contact_list = AddressBook()
 
 
-def input_error(func):
-    def wrapper(*args):
-        try:
-            return func(*args)
-        except KeyError:
-            return "Contact not found."
-        except ValueError:
-            return "ValueError. Try again"
-        except IndexError:
-            return "IndexError. Try again"
-        except NameError:
-            return "Invalid input. Name should contain only letters."
-        except TypeError:
-            return "Invalid input. Try again"
-    return wrapper
+class CommandError(Exception):
+    pass
 
 
-# @input_error
-def command_add():
-    name = Name(input("Введіть ім'я: ").title())
-    phone = Phone(input('Введіть номер: '))
-    birthday = Birthday(input('Введіть дату народження: '))
-    email = Email(input('Введіть email-пошту: '))
-    adress = Adress(input('Введіть адрессу: '))
-    contacts = Record(name, phone, birthday, email, adress)
-    contact_list.add_record(contacts)
-    return f"Contact {name} has been added."
+class Command(ABC):
+    @abstractmethod
+    def execute(self, *args):
+        raise NotImplementedError
 
 
-@input_error
-def command_delete(input_str):
-    _, name = input_str.split()
-    contact_list.delete_record(name.title())
-    return f'Contact {name.title()} succefully deleted'
+class AddCommand(Command):
+    def execute(self, name, phone, birthday, email, address):
+        name = NameField(name)
+        phone = PhoneField(phone)
+        birthday = BirthdayField(birthday)
+        email = EmailField(email)
+        address = AddressField(address)
+        contacts = Record(name, phone, birthday, email, address)
+        contact_list.add_record(contacts)
+        return f"Contact {name} has been added."
 
 
-@input_error
-def command_change():
-    name = Name(input("Введіть ім'я: ").title())
-    phone = Phone(input('Введіть номер: '))
-    birthday = Birthday(input('Введіть дату народження: '))
-    email = Email(input('Введіть email-пошту: '))
-    adress = Adress(input('Введіть адрессу: '))
-    update = Record(name, phone, birthday, email, adress)
-    contact_list.update_record(update)
-    return f"Contact {name} has been updated."
+class DeleteCommand(Command):
+    def execute(self, name):
+        _, name = name.title()
+        contact_list.delete_record(name)
+        return f'Contact {name} successfully deleted'
 
 
-@input_error
-def command_search(input_str):
-    _, name = input_str.split()
-    result = contact_list.find_record(name.title())
-    return result.name.value, result.phone.value, str(result.birthday.value), result.email.value, result.adress.value
+class ChangeCommand(Command):
+    def execute(self, name, phone, birthday, email, address):
+        name = NameField(name)
+        phone = PhoneField(phone)
+        birthday = BirthdayField(birthday)
+        email = EmailField(email)
+        address = AddressField(address)
+        update = Record(name, phone, birthday, email, address)
+        contact_list.add_record(update)
+        return f"Contact {name} has been updated."
 
 
-def command_show_all(contact_list):
+class SearchCommand(Command):
+    def execute(self, name):
+        _, name = name.title()
+        result = contact_list.get(name, None)
+        if result:
+            return result.name.value, result.phones[0].value, str(result.birthday.value), result.email.value, result.address.value
+        return "Contact not found."
+
+
+class ShowAllCommand(Command):
+    def execute(self):
+        command_show_all()
+
+
+def command_show_all():
     if not contact_list:
-        return "Список контактів пустий."
+        return "The contact list is empty."
     result = "Contacts:"
     print(result)
     print('------------------------------------------------------------------------------------------------------------------')
-    print('Name          |     Number     |     Birthday     |            Email             |             Adress            |')
+    print('Name          |     Number     |     Birthday     |            Email             |             Address           |')
     for name, value in contact_list.items():
         print('--------------|----------------|------------------|------------------------------|-------------------------------|')
-        print('{:<14}|{:^16}|{:^18}|{:^30}|{:^30} |'.format(name, value.phone.value, str(
-            value.birthday.value), value.email.value, value.adress.value))
+        print('{:<14}|{:^16}|{:^18}|{:^30}|{:^30} |'.format(name, value.phones[0].value, str(
+            value.birthday.value), value.email.value, value.address.value))
     print('------------------------------------------------------------------------------------------------------------------')
 
 
-@input_error
+class DaysToBirthdayCommand(Command):
+    def execute(self, days):
+        _, days = int(days)
+        result = command_days_to_birthday(days)
+        return result
+
+
+# @input_error
 def command_days_to_birthday(input_str):
-    result = ''
     _, days = input_str.split()
-    d_now = datetime.now().date()
+    try:
+        days = int(days)
+        if days <= 0:
+            raise ValueError
+    except ValueError:
+        raise ValueError("Invalid number of days")
+
+    result = ''
+    today = datetime.now().date()
     for key, value in contact_list.items():
-        birthday = value.birthday.value
-        birthday = birthday.replace(year=d_now.year)
-        days_to_br = timedelta(days=int(days))
-        days_to_br = d_now + days_to_br
-        if d_now <= birthday <= days_to_br:
-            result += f'{key} have birthday in next {days} days. {value.birthday}\n'
-        else:
-            continue
-    return result.strip() if result else f'No birthdays in next {days} days'
+        birthday = value.birthday.value.replace(year=today.year)
+        next_birthday = birthday.replace(year=today.year + 1)
+        if today <= birthday <= (today + timedelta(days=days)) or today <= next_birthday <= (today + timedelta(days=days)):
+            result += f'{key} has a birthday in the next {days} days. ({value.birthday})\n'
+
+    return result.strip() if result else f'No birthdays in the next {days} days'
+
 
 def main():
-    if os.path.exists('AdressBook.bin'):
+    if os.path.exists('AddressBook.bin'):
         contact_list.load()
-    print("Доступні команди:'hello','add','change', 'delete', 'search', 'birthday', 'show all','good bye','close','exit'")
+    print("Available commands: 'hello', 'add', 'change', \
+          'delete', 'search', 'birthday', 'show all', 'good bye', 'close', 'exit'")
+
     while True:
-        input_str = prompt("Enter your command: ",completer=Completer, lexer=RainbowLetter())
+        input_str = prompt("Enter your command: ", completer=Completer, lexer=RainbowLetter())
+        command = None
 
         if input_str == "hello":
             print("How can I help you?")
         elif input_str.startswith("add"):
-            print(command_add())
+            command = AddCommand()
         elif input_str.startswith("change"):
-            print(command_change())
+            command = ChangeCommand()
         elif input_str.startswith("delete "):
-            print(command_delete(input_str))
+            command = DeleteCommand()
         elif input_str.startswith("search "):
-            print(command_search(input_str))
+            command = SearchCommand()
         elif input_str.startswith("birthday "):
-            print(command_days_to_birthday(input_str))
+            command = DaysToBirthdayCommand()
         elif input_str == "show all":
-            command_show_all(contact_list)
+            command = ShowAllCommand()
         elif input_str in ["good bye", "close", "exit"]:
             print("Good bye!")
             break
         else:
-            print("Невірно введена команда. Доступні команди:'hello','add','change','phone','show all','good bye','close','exit'")
-        contact_list.dump()
+            print("Invalid command. Available commands: 'hello',\
+                   'add', 'change', 'delete', 'search', 'birthday', 'show all', 'good bye', 'close', 'exit'")
+
+        if command:
+            try:
+                result = command.execute(*input_str.split())
+                print(result)
+                contact_list.dump()
+            except CommandError as e:
+                print(e)
 
 
 if __name__ == "__main__":
     main()
-
