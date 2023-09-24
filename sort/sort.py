4f6eff68b38d8f2a9c7a26cbacbaa730d50ad08a
DSofiya <134394822+DSofiya@users.noreply.github.com>
2023-09-24 12:33:50 +0300

    Update sort.py

--- sort/sort.py
+++ sort/sort.py
@@ -1,97 +1,117 @@
 import os
 import shutil
+import zipfile
+from abc import ABC, abstractmethod
+from rarfile import RarFile
 
-class FileSorter:
-    def __init__(self, path):
-        self.path = path
-        self.extensions = {
-            'images': ('.jpg', '.png', '.jpeg', '.svg'),
-            'videos': ('.avi', '.mp4', '.mov', '.mkv'),
-            'documents': ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'),
-            'music': ('.mp3', '.ogg', '.wav', '.amr'),
-            'archives': ('.zip', '.gz', '.tar'),
-            'python' : ('.py')
-        }
+class ExtensionStrategy(ABC):
+    @abstractmethod
+    def process_file(self, file_sorter, root, file):
+        pass
 
+    @abstractmethod
+    def print_results(self, file_sorter):
+        pass
+
+class DefaultExtensionStrategy(ExtensionStrategy):
+    extensions = {
+        'images': ('.jpg', '.png', '.jpeg', '.svg'),
+        'videos': ('.avi', '.mp4', '.mov', '.mkv'),
+        'documents': ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'),
+        'music': ('.mp3', '.ogg', '.wav', '.amr'),
+        'archives': ('.zip', '.gz', '.tar', '.rar'),  # Додано підтримку .rar
+        'python': ('.py')
+    }
+
+    def __init__(self):
         self.unknown_extensions = set()
+        self.for_print = {key: [] for key in self.extensions.keys()}
 
-        self.for_print = {
-            'images': [],
-            'videos': [],
-            'documents': [],
-            'music': [],
-            'archives': [],
-            'python' : []
-        }
+    def process_file(self, file_sorter, root, file):
+        filename, extension = os.path.splitext(file)
+        found = False
+        for folder, exts in self.extensions.items():
+            if extension.lower() in exts:
+                new_path = os.path.join(root, folder, file_sorter.normalize(file))
+                os.makedirs(os.path.dirname(new_path), exist_ok=True)
+                shutil.move(os.path.join(root, file), new_path)
+                found = True
+                self.add_and_print_extensions(file_sorter, folder, extension.lower())
+                break
+        if not found:
+            self.add_and_print_extensions(file_sorter, None, extension.lower())
+
+    def add_and_print_extensions(self, file_sorter, folder, extension):
+        if folder in file_sorter.for_print:
+            if extension not in file_sorter.for_print[folder]:
+                file_sorter.for_print[folder].append(extension)
+        else:
+            file_sorter.unknown_extensions.add(extension)
+
+    def print_results(self, file_sorter):
+        print('Знайдені розширення:')
+        for folder, extensions in file_sorter.for_print.items():
+            if extensions:
+                print(f'{folder}: {", ".join(extensions)}')
+
+        print('Невідомі розширення:')
+        print(', '.join(file_sorter.unknown_extensions))
+        print('Сортування завершено. Вихід у головне меню')
+
+class ZipExtensionStrategy(ExtensionStrategy):
+    def process_file(self, file_sorter, root, file):
+        filename, extension = os.path.splitext(file)
+        if extension.lower() in file_sorter.extensions['archives']:
+            file_sorter.for_print['archives'].append(extension.lower())
+            self.extract_zip(os.path.join(root, file), os.path.join(root, file_sorter.normalize(filename)))
+        elif extension.lower() == '.rar':  # Додано підтримку .rar
+            file_sorter.for_print['archives'].append(extension.lower())
+            self.extract_rar(os.path.join(root, file), os.path.join(root, file_sorter.normalize(filename)))
+
+    def extract_zip(self, source, destination):
+        with zipfile.ZipFile(source, 'r') as zip_ref:
+            zip_ref.extractall(destination)
+        os.remove(source)
+
+    def extract_rar(self, source, destination):
+        with RarFile(source, 'r') as rar_ref:
+            rar_ref.extractall(destination)
+        os.remove(source)
 
     def normalize(self, name):
         return ''.join(c for c in name if c.isalnum() or c in [' ', '.', '_']).rstrip()
-    #нормалізує ім;я
 
-    def add_and_print_extensions(self, folder, extension):
-        if folder in self.for_print:
-            if extension not in self.for_print[folder]:
-                self.for_print[folder].append(extension)
-        else:
-            self.unknown_extensions.add(extension)
-    #якщо розширення не в фор_прінт, розширення перекидається до фор_прінт
+    def print_results(self, file_sorter):
+        pass
+
+class FileSorter:
+    def __init__(self, path, extension_strategy=None):
+        self.path = path
+        self.extension_strategy = extension_strategy or DefaultExtensionStrategy()
+        self.unknown_extensions = set()
+        self.for_print = {key: [] for key in self.extension_strategy.extensions.keys()}
 
     def sort_files(self):
         for root, dirs, files in os.walk(self.path):
             for folder in dirs:
-                if folder.lower() in self.extensions.keys():
+                if folder.lower() in self.extension_strategy.extensions.keys():
                     dirs.remove(folder)
 
             for file in files:
-                filename, extension = os.path.splitext(file)
-                found = False
-                for folder, exts in self.extensions.items():
-                    if extension.lower() in exts:
-                        new_path = os.path.join(root, folder, self.normalize(file))
-                        os.makedirs(os.path.dirname(new_path), exist_ok=True)
-                        shutil.move(os.path.join(root, file), new_path)
-                        found = True
-                        self.add_and_print_extensions(folder, extension.lower())
-                        break
-                if not found:
-                    self.unknown_extensions.add(extension.lower())
-    #основна частина коду, створюємо нову папку відповідно до розширення папки та перекидуєм її туди, потім викликаємо функцію адд_енд_прінт, ти визначаємо що далі робити з розширенням, або перекидаємо його до невідомих
-
-        for folder in ['archives']:
-            for root, dirs, files in os.walk(os.path.join(self.path, folder)):
-                for file in files:
-                    filename, extension = os.path.splitext(file)
-    #розділяємо ім'я та розширення за допомгою функцій ос
-
-                    if extension.lower() in self.extensions['archives']:
-                        self.for_print['archives'].append(extension.lower())
-    #додаємо в знайдені розширення, знайдений архів
-
-                    if extension.lower() == '.zip':
-                        new_folder = os.path.join(root, self.normalize(filename))
-                        os.makedirs(new_folder, exist_ok=True)
-                        shutil.unpack_archive(os.path.join(root, file), new_folder)
-                        os.remove(os.path.join(root, file))
-    #розпаковуємо архів за допомогою функцій шутіл та ос
+                self.extension_strategy.process_file(self, root, file)
 
     def print_results(self):
-        print('Знайдені розширення:')
-        for folder, extensions in self.for_print.items():
-            if extensions:
-                print(f'{folder}: {", ".join(extensions)}')
-        
-        print('Невідомі розширення:')
-        print(', '.join(self.unknown_extensions))
-    
+        self.extension_strategy.print_results(self)
+
+    def normalize(self, name):
+        return ''.join(c for c in name if c.isalnum() or c in [' ', '.', '_']).rstrip()
+
 def main():
-    while True:    
-        print("Для відміни введіть пусту строку або 'cancel'. ")
-        path = str(input("Шлях до папки ==> (C:|Users|Oleg|Documents|some_rubbish): "))
-        if path == '' or path == 'cancel':
-            break
-        sorter = FileSorter(path)
-        sorter.sort_files()
-        sorter.print_results()
+    print('Приклад - C:\Users\Admin\Documents\trash')
+    path = input("Шлях до папки ==>")
+    sorter = FileSorter(path)
+    sorter.sort_files()
+    sorter.print_results()
 
 if __name__ == "__main__":
     main()
